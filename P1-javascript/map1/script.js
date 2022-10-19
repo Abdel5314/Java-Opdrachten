3b7f320773a18d98295f047df1d5ed7c41900fd3
Abdel5314 <113344711+Abdel5314@users.noreply.github.com>
2022-10-19 10:42:33 +0200

    Add files via upload

---
+++ P1-javascript/map1/script.js
@@ -0,0 +1,18 @@
+alert("Hallo wereld!")
+document.getElementById("title").style.color = "red";
+
+
+
+/*
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert("Het atwoord is: " + som)
+
+*/
+
+//promtbox
+var kleur = prompt("Geef een engelse kleurnaam: (red of blue)", "");
+
+//kleur aanpassen
+
